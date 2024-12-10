1fe967b1ca83b03cba6430358265ba076e568922
Peter Atef <peter.atef@scandiweb.com>
2024-12-10 17:50:05 +0200

    Added copilot and status line

--- init.lua
+++ init.lua
@@ -239,6 +239,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	{
+		"nvim-lualine/lualine.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 	"JoosepAlviste/palenightfall.nvim",
 	{
 		"kdheepak/lazygit.nvim",
@@ -430,6 +437,47 @@ require("lazy").setup({
 					background = "#1C202D",
 				},
 				highlight_overrides = {},
+			})
+
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = "palenight",
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					always_divide_middle = true,
+					always_show_tabline = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 100,
+						tabline = 100,
+						winbar = 100,
+					},
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { "filename" },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {},
 			})
 
 			-- Enable Telescope extensions if they are installed
