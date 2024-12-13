d528cda709f0c71550ce132b08f3c1f25d676365
Peter Atef <peter.atef@scandiweb.com>
2024-12-13 12:55:08 +0200

    Recreated from dillon's config

---
+++ lua/plugins/color_scheme.lua
@@ -0,0 +1,49 @@
+return {
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					cmp = true,
+					fidget = true,
+					gitsigns = true,
+					harpoon = true,
+					indent_blankline = {
+						enabled = false,
+						scope_color = "sapphire",
+						colored_indent_levels = false,
+					},
+					mason = true,
+					native_lsp = { enabled = true },
+					noice = true,
+					notify = true,
+					symbols_outline = true,
+					telescope = true,
+					treesitter = true,
+					treesitter_context = true,
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin-mocha")
+
+			-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+			for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+				vim.api.nvim_set_hl(0, group, {})
+			end
+		end,
+	},
+	-- {
+	-- 	"JoosepAlviste/palenightfall.nvim",
+	-- 	setup = function()
+	-- 		require("palenightfall").setup({
+	-- 			transparent = false,
+	-- 			color_overrides = {
+	-- 				background = "#1C202D",
+	-- 			},
+	-- 			highlight_overrides = {},
+	-- 		})
+	-- 	end,
+	-- },
+}
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,422 @@
+local nnoremap = require("user.keymap_utils").nnoremap
+local vnoremap = require("user.keymap_utils").vnoremap
+local inoremap = require("user.keymap_utils").inoremap
+local tnoremap = require("user.keymap_utils").tnoremap
+local xnoremap = require("user.keymap_utils").xnoremap
+local harpoon_ui = require("harpoon.ui")
+local harpoon_mark = require("harpoon.mark")
+local conform = require("conform")
+local utils = require("user.utils")
+
+local M = {}
+
+-- Normal --
+-- Disable Space bar since it'll be used as the leader key
+nnoremap("<space>", "<nop>")
+
+-- Window +  better kitty navigation
+nnoremap("<C-j>", function()
+	if vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
+		vim.cmd.NvimTmuxNavigateDown()
+	else
+		vim.cmd.wincmd("j")
+	end
+end)
+
+nnoremap("<C-k>", function()
+	if vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
+		vim.cmd.NvimTmuxNavigateUp()
+	else
+		vim.cmd.wincmd("k")
+	end
+end)
+
+nnoremap("<C-l>", function()
+	if vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
+		vim.cmd.NvimTmuxNavigateRight()
+	else
+		vim.cmd.wincmd("l")
+	end
+end)
+
+nnoremap("<C-h>", function()
+	if vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
+		vim.cmd.NvimTmuxNavigateLeft()
+	else
+		vim.cmd.wincmd("h")
+	end
+end)
+
+-- Swap between last two buffers
+nnoremap("<leader>'", "<C-^>", { desc = "Switch to last buffer" })
+
+-- Save with leader key
+nnoremap("<leader>w", "<cmd>w<cr>", { silent = false, desc = "Write to buffer" })
+
+-- Quit with leader key
+nnoremap("<leader>q", "<cmd>q<cr>", { silent = false, desc = "[Q]uit" })
+
+-- Save and Quit with leader key
+nnoremap("<leader>z", "<cmd>wq<cr>", { silent = false, desc = "Save and quit" })
+
+-- Map Oil to <leader>e
+nnoremap("<leader>e", function()
+	require("oil").toggle_float()
+end, { desc = "Open Oil" })
+
+-- Map Undotree to <leader>
+nnoremap("<leader>ut", ":UndotreeToggle<CR>", { desc = "Toggle [U]ndo[T]ree " })
+
+-- Center buffer while navigating
+nnoremap("<C-u>", "<C-u>zz")
+nnoremap("<C-d>", "<C-d>zz")
+nnoremap("{", "{zz")
+nnoremap("}", "}zz")
+nnoremap("N", "Nzz")
+nnoremap("n", "nzz")
+nnoremap("G", "Gzz")
+nnoremap("gg", "ggzz")
+nnoremap("gd", "gdzz")
+nnoremap("<C-i>", "<C-i>zz")
+nnoremap("<C-o>", "<C-o>zz")
+nnoremap("%", "%zz")
+nnoremap("*", "*zz")
+nnoremap("#", "#zz")
+
+-- Press 'S' for quick find/replace for the word under the cursor
+nnoremap("S", function()
+	local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
+	local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
+	vim.api.nvim_feedkeys(keys, "n", false)
+end)
+
+-- Open Spectre for global find/replace
+nnoremap("<leader>S", function()
+	require("spectre").toggle()
+end, { desc = "Open spectre for global find/replace" })
+
+-- Open Spectre for global find/replace for the word under the cursor in normal mode
+-- TODO Fix, currently being overriden by Telescope
+nnoremap("<leader>sw", function()
+	require("spectre").open_visual({ select_word = true })
+end, { desc = "Search current word" })
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+nnoremap("L", "$")
+nnoremap("H", "^")
+
+-- Press 'U' for redo
+nnoremap("U", "<C-r>")
+
+-- Turn off highlighted results
+nnoremap("<leader>no", "<cmd>noh<cr>")
+
+-- Diagnostics
+
+-- Goto next diagnostic of any severity
+nnoremap("]d", function()
+	vim.diagnostic.goto_next({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous diagnostic of any severity
+nnoremap("[d", function()
+	vim.diagnostic.goto_prev({})
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next error diagnostic
+nnoremap("]e", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous error diagnostic
+nnoremap("[e", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto next warning diagnostic
+nnoremap("]w", function()
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Goto previous warning diagnostic
+nnoremap("[w", function()
+	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+	vim.api.nvim_feedkeys("zz", "n", false)
+end)
+
+-- Moonlander diagnostic movements --
+
+-- Open the diagnostic under the cursor in a float window
+nnoremap("<leader>d", function()
+	vim.diagnostic.open_float({
+		border = "rounded",
+	})
+end, { desc = "Open [d]iagnostic under the curstor" })
+
+-- Place all dignostics into a qflist
+nnoremap("<leader>ld", vim.diagnostic.setqflist, { desc = "Quickfix [L]ist [D]iagnostics" })
+
+-- Navigate to next qflist item
+nnoremap("<leader>cn", ":cnext<cr>zz", { desc = "Navigate to next qflist item" })
+
+-- Navigate to previos qflist item
+nnoremap("<leader>cp", ":cprevious<cr>zz", { desc = "Navigate to previous qflist item" })
+
+-- Open the qflist
+nnoremap("<leader>co", ":copen<cr>zz", { desc = "Open the qflist" })
+
+-- Close the qflist
+nnoremap("<leader>cc", ":cclose<cr>zz", { desc = "Close the qflist" })
+
+-- Map MaximizerToggle (szw/vim-maximizer) to leader-m
+nnoremap("<leader>m", ":MaximizerToggle<cr>", { desc = "Toggle [M]aximizer" })
+
+-- Resize split windows to be equal size
+nnoremap("<leader>=", "<C-w>=", { desc = "Resize split windows to be equal size" })
+
+-- Press leader f to format
+nnoremap("<leader>f", function()
+	conform.format({
+		async = true,
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	})
+end, { desc = "[F]ormat the buffer" })
+
+-- Press leader rw to rotate open windows
+nnoremap("<leader>rw", ":RotateWindows<cr>", { desc = "[R]otate [W]indows" })
+
+-- Press gx to open the link under the cursor
+nnoremap("gx", ":sil !open <cWORD><cr>", { silent = true, desc = "Open link under cursor" })
+
+-- TSC autocommand keybind to run TypeScripts tsc
+nnoremap("<leader>tc", ":TSC<cr>", { desc = "[T]ypeScript [C]ompile" })
+
+-- Harpoon keybinds --
+-- Open harpoon ui
+nnoremap("<leader>ho", function()
+	harpoon_ui.toggle_quick_menu()
+end, { desc = "[O]pen [H]arpoon" })
+
+-- Add current file to harpoon
+nnoremap("<leader>ha", function()
+	harpoon_mark.add_file()
+end, { desc = "[H]arpoon [A]dd" })
+
+-- Remove current file from harpoon
+nnoremap("<leader>hr", function()
+	harpoon_mark.rm_file()
+end, { desc = "[H]arpoon [R]emove" })
+
+-- Remove all files from harpoon
+nnoremap("<leader>hc", function()
+	harpoon_mark.clear_all()
+end, { desc = "[H]arpoon [C]lear" })
+
+-- Quickly jump to harpooned files
+nnoremap("<leader>1", function()
+	harpoon_ui.nav_file(1)
+end, { desc = "Jump to harpooned file 1" })
+
+nnoremap("<leader>2", function()
+	harpoon_ui.nav_file(2)
+end, { desc = "Jump to harpooned file 2" })
+
+nnoremap("<leader>3", function()
+	harpoon_ui.nav_file(3)
+end, { desc = "Jump to harpooned file 3" })
+
+nnoremap("<leader>4", function()
+	harpoon_ui.nav_file(4)
+end, { desc = "Jump to harpooned file 4" })
+
+nnoremap("<leader>5", function()
+	harpoon_ui.nav_file(5)
+end, { desc = "Jump to harpooned file 5" })
+
+-- Git keymaps --
+nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
+nnoremap("<leader>gf", function()
+	local cmd = {
+		"sort",
+		"-u",
+		"<(git diff --name-only --cached)",
+		"<(git diff --name-only)",
+		"<(git diff --name-only --diff-filter=U)",
+	}
+
+	if not utils.is_git_directory() then
+		vim.notify(
+			"Current project is not a git directory",
+			vim.log.levels.WARN,
+			{ title = "Telescope Git Files", git_command = cmd }
+		)
+	else
+		require("telescope.builtin").git_files()
+	end
+end, { desc = "Search [G]it [F]iles" })
+
+-- Telescope keybinds --
+nnoremap("<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
+nnoremap("<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch Open [B]uffers" })
+nnoremap("<leader>sf", function()
+	require("telescope.builtin").find_files({ hidden = true })
+end, { desc = "[S]earch [F]iles" })
+nnoremap("<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+nnoremap("<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+
+nnoremap("<leader>sc", function()
+	require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
+end, { desc = "[S]earch [C]ommands" })
+
+nnoremap("<leader>/", function()
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
+end, { desc = "[/] Fuzzily search in current buffer]" })
+
+nnoremap("<leader>ss", function()
+	require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
+end, { desc = "[S]earch [S]pelling suggestions" })
+
+-- LSP Keybinds (exports a function to be used in ../../after/plugin/lsp.lua b/c we need a reference to the current buffer) --
+M.map_lsp_keybinds = function(buffer_number)
+	nnoremap("<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame", buffer = buffer_number })
+	nnoremap("<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction", buffer = buffer_number })
+
+	nnoremap("gd", vim.lsp.buf.definition, { desc = "LSP: [G]oto [D]efinition", buffer = buffer_number })
+
+	-- Telescope LSP keybinds --
+	nnoremap(
+		"gr",
+		require("telescope.builtin").lsp_references,
+		{ desc = "LSP: [G]oto [R]eferences", buffer = buffer_number }
+	)
+
+	nnoremap(
+		"gi",
+		require("telescope.builtin").lsp_implementations,
+		{ desc = "LSP: [G]oto [I]mplementation", buffer = buffer_number }
+	)
+
+	nnoremap(
+		"<leader>bs",
+		require("telescope.builtin").lsp_document_symbols,
+		{ desc = "LSP: [B]uffer [S]ymbols", buffer = buffer_number }
+	)
+
+	nnoremap(
+		"<leader>ps",
+		require("telescope.builtin").lsp_workspace_symbols,
+		{ desc = "LSP: [P]roject [S]ymbols", buffer = buffer_number }
+	)
+
+	-- See `:help K` for why this keymap
+	-- nnoremap("K", vim.lsp.buf.hover, { desc = "LSP: Hover Documentation", buffer = buffer_number })
+	nnoremap("<leader>k", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+	inoremap("<C-k>", vim.lsp.buf.signature_help, { desc = "LSP: Signature Documentation", buffer = buffer_number })
+
+	-- Lesser used LSP functionality
+	nnoremap("gD", vim.lsp.buf.declaration, { desc = "LSP: [G]oto [D]eclaration", buffer = buffer_number })
+	nnoremap("td", vim.lsp.buf.type_definition, { desc = "LSP: [T]ype [D]efinition", buffer = buffer_number })
+end
+
+-- Symbol Outline keybind
+nnoremap("<leader>so", ":SymbolsOutline<cr>", { desc = "[S]ymbol [O]utline" })
+
+-- Open Copilot panel
+nnoremap("<leader>oc", function()
+	require("copilot.panel").open({})
+end, { desc = "[O]pen [C]opilot panel" })
+
+-- nvim-ufo keybinds
+-- nnoremap("zR", require("ufo").openAllFolds)
+-- nnoremap("zM", require("ufo").closeAllFolds)
+
+-- toggle inlay hints
+nnoremap("<leader>ih", function()
+	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({}))
+end)
+
+-- Insert --
+-- Map jj and JJ to <esc>
+--inoremap("jj", "<esc>")
+--inoremap("JJ", "<esc>")
+
+-- Visual --
+-- Disable Space bar since it'll be used as the leader key
+vnoremap("<space>", "<nop>")
+
+-- Press 'H', 'L' to jump to start/end of a line (first/last char)
+vnoremap("L", "$<left>")
+vnoremap("H", "^")
+
+vnoremap("<A-j>", ":m '>+1<CR>gv=gv")
+vnoremap("<A-k>", ":m '<-2<CR>gv=gv")
+
+-- Paste without losing the contents of the register
+xnoremap("<leader>p", '"_dP', { desc = "Paste without losing the contents of the register" })
+
+-- Reselect the last visual selection
+xnoremap("<<", function()
+	-- Move selected text up/down in visual mode
+	vim.cmd("normal! <<")
+	vim.cmd("normal! gv")
+end)
+
+xnoremap(">>", function()
+	vim.cmd("normal! >>")
+	vim.cmd("normal! gv")
+end)
+
+-- Terminal --
+-- Enter normal mode while in a terminal
+tnoremap("<esc>", [[<C-\><C-n>]])
+--tnoremap("jj", [[<C-\><C-n>]])
+
+-- Window navigation from terminal
+tnoremap("<C-h>", [[<Cmd>wincmd h<CR>]])
+tnoremap("<C-j>", [[<Cmd>wincmd j<CR>]])
+tnoremap("<C-k>", [[<Cmd>wincmd k<CR>]])
+tnoremap("<C-l>", [[<Cmd>wincmd l<CR>]])
+
+-- Reenable default <space> functionality to prevent input delay
+tnoremap("<space>", "<space>")
+
+-- My key remaps
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<C-z>", "u")
+vim.keymap.set("i", "<C-z>", vim.cmd.undo, { desc = "Undo" })
+vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>")
+vim.keymap.set("i", "<C-s>", "<cmd>:w<cr>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Close window" })
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<C-`>", ":FloatermNew<CR>", { desc = "New floaterm window" })
+vim.keymap.set("n", "<C-j>", "<cmd>:FloatermToggle<cr>", { desc = "New floaterm window" })
+vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<cr>", { desc = "[T]erminal [T]oggle" })
+vim.keymap.set("t", "<C-j>", "<cmd>:FloatermToggle<cr>", { desc = "[T]erminal [T]oggle" })
+vim.keymap.set("i", "<C-c>", "<esc>", { desc = "Close insert mode" })
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
+vim.keymap.set("n", "<leader>vs", "<cmd>vs<cr>", { desc = "[V]ertical [S]plit" })
+vim.keymap.set("n", "<leader>vn", "<cmd>vnew<cr>", { desc = "[V]ertical [N]ew" })
+vim.keymap.set("n", "K", ":m .-2<CR>==", { desc = "Move current line up" })
+vim.keymap.set("n", "J", ":m .+1<CR>==", { desc = "Move current line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
+
+return M
